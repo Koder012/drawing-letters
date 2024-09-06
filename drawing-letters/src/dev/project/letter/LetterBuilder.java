2c9f90ca7c1beef0b8f37e1c4a3f3091f7da5152
josue.dosidiana <josue.dosidiana@gmail.com>
2024-09-06 22:09:43 +0100

    update:implementei a função que imprime a letra 'A'.

---
+++ drawing-letters/src/dev/project/letter/LetterBuilder.java
@@ -0,0 +1,17 @@
+package dev.project.letter;
+
+public class LetterBuilder {
+
+    public static void buildingLetterA(){
+        for(int i = 1; i <= 5;i++){
+            for(int j = 1; j <= 9;j++){
+                if((j == 6-i) || (i==3 && j>=3 && j<= 7) || (j == 4+i))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ drawing-letters/src/dev/project/letter/Tester.java
@@ -0,0 +1,8 @@
+package dev.project.letter;
+
+public class Tester {
+    public static void main(String[] args){
+        LetterBuilder.buildingLetterA();
+
+    }
+}
